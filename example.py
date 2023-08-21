bdd4a50d749dfa1a3255170d650aee1cedd9023f
maksimov.yd <maksimov.yd@gmail.com>
2023-08-21 15:18:07 +0300

    add git actions

--- example.py
+++ example.py
@@ -13,6 +13,15 @@ def reverse(string):
 
 
 # Нужно для запуска тестов
+# 1) python3 -i example.py
+# 2) >>> reverse('')
+# ''
+# >>> reverse('Hexlet')
+# 'telxeH'
+#if __name__ == "__main__":
+    # import doctest
+    # doctest.testmod()
+# 3) python3 example.py -v
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
